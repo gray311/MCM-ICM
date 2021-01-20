d874706e0f228ff27ed44d7fd75e56b0ccc3e1bc
Yukinoshita Yukino <64787301+gray311@users.noreply.github.com>
2021-01-20 22:58:34 +0800

    Add files via upload

--- matlpotlib.py
+++ matlpotlib.py
@@ -27,5 +27,23 @@ plt.xticks(new_ticks)
 
 # set tick labels
 
-plt.yticks([-2, -1.8, -1, 1.22, 3],[r'$really\ bad$', r'$bad$', r'$normal$', r'$good$', r'$really\ good$'])
+plt.yticks([-2, -1.8, -1, 1.22, 3], [r'$really\ bad$', r'$bad$', r'$normal$', r'$good$', r'$really\ good$'])
+
+# gca = 'get current axis'
+ax = plt.gca()
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+
+ax.xaxis.set_ticks_position('bottom')
+# ACCEPTS: [ 'top' | 'bottom' | 'both' | 'default' | 'none' ]
+
+ax.spines['bottom'].set_position(('data', 0))
+# the 1st is in 'outward' | 'axes' | 'data'
+# axes: percentage of y axis
+# data: depend on y data
+
+ax.yaxis.set_ticks_position('left')
+# ACCEPTS: [ 'left' | 'right' | 'both' | 'default' | 'none' ]
+
+ax.spines['left'].set_position(('data',0))
 plt.show()
