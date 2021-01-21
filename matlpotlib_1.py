0b65d6bd62f5997ce89be8794bd273eb38ab4210
Yukinoshita Yukino <64787301+gray311@users.noreply.github.com>
2021-01-21 19:55:23 +0800

    Add files via upload

--- matlpotlib_1.py
+++ matlpotlib_1.py
@@ -27,7 +27,8 @@ plt.xticks(new_ticks)
 
 # set tick labels
 
-plt.yticks([-2, -1.8, -1, 1.22, 3], [r'$really\ bad$', r'$bad$', r'$normal$', r'$good$', r'$really\ good$'])
+plt.yticks([-2, -1.8, -1, 1.22, 3],
+           [r'$really\ bad$', r'$bad$', r'$normal$', r'$good$', r'$really\ good$'])
 
 # gca = 'get current axis'
 ax = plt.gca()
@@ -50,16 +51,21 @@ plt.scatter([x0, ], [y0, ], s=50, color='b')
 
 # method 1:
 #####################
-plt.annotate(r'$2x+1=%s$' % y0, xy=(x0, y0), xycoords='data', xytext=(+30, -30),textcoords='offset points', fontsize=16,arrowprops=dict(arrowstyle='->', connectionstyle="arc3,rad=.2"))
+plt.annotate(r'$2x+1=%s$' % y0, xy=(x0, y0), xycoords='data', xytext=(+30, -30),
+             textcoords='offset points', fontsize=16, arrowprops=dict(arrowstyle='->',
+             connectionstyle="arc3,rad=.2"))
 
 # method 2:
 ########################
-plt.text(-3.7, 3, r'$This\ is\ the\ some\ text. \mu\ \sigma_i\ \alpha_t$', fontdict={'size': 16, 'color': 'r'})
+plt.text(-3.7, 3, r'$This\ is\ the\ some\ text. \mu\ \sigma_i\ \alpha_t$',
+         fontdict={'size': 16, 'color': 'r'})
 
 #ticks vision
 for label in ax.get_xticklabels() + ax.get_yticklabels():
     label.set_fontsize(12)
     # set zorder for ordering the plot in plt 2.0.2 or higher
-    label.set_bbox(dict(facecolor='red', edgecolor='none', alpha=0.7, zorder=2))
+    label.set_bbox(dict(facecolor='red',
+                   edgecolor='none', alpha=0.7, zorder=2))
+
 
 plt.show()
