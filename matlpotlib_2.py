01a2e105def40ed839a305655f9208c57edf8c91
Yukinoshita Yukino <64787301+gray311@users.noreply.github.com>
2021-01-22 16:57:26 +0800

    Add files via upload

--- matlpotlib_2.py
+++ matlpotlib_2.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 
 #散点图
 n = 1024    # data size
@@ -14,8 +15,12 @@ plt.xticks(())  # ignore xticks
 plt.ylim(-1.5, 1.5)
 plt.yticks(())  # ignore yticks
 
+plt.show()
+
 plt.figure()
+
 #柱状图
+
 n = 12
 X = np.arange(n)
 Y1 = (1 - X / float(n)) * np.random.uniform(0.5, 1.0, n)
@@ -28,8 +33,6 @@ for x, y in zip(X, Y1):
     # ha: horizontal alignment
     # va: vertical alignment
     plt.text(x + 0.05, y + 0.02, '%.2f' % y, ha='center', va='bottom', size=8)
-    
-
 
 '''for x, y in zip(X, Y2):
     # ha: horizontal alignment
@@ -41,6 +44,64 @@ plt.xticks(())
 plt.ylim(-1.25, 1.25)
 plt.yticks(())
 
-plt.show()
+plt.figure()
+
+#等高线
+
+def f(x,y):
+    # the height function
+    return (1 - x / 2 + x**5 + y**3) * np.exp(-x**2 -y**2)
+
+n = 256
+x = np.linspace(-3, 3, n)
+y = np.linspace(-3, 3, n)
+X,Y = np.meshgrid(x, y)
+
+# use plt.contourf to filling contours
+# X, Y and value for (X,Y) point
+plt.contourf(X, Y, f(X, Y), 8, alpha=.75, cmap=plt.cm.hot)
+
+# use plt.contour to add contour lines
+C = plt.contour(X, Y, f(X, Y), 8, colors='black', linewidth=.5)
+# adding label
+plt.clabel(C, inline=True, fontsize=10)
+
+plt.xticks(())
+plt.yticks(())
+
+
+plt.figure()
+
+# image data
+a = np.array([0.313660827978, 0.365348418405, 0.423733120134,
+              0.365348418405, 0.439599930621, 0.525083754405,
+              0.423733120134, 0.525083754405, 0.651536351379]).reshape(3,3)
+
+plt.imshow(a, interpolation='nearest', cmap='bone', origin='lower')
+plt.colorbar(shrink=.92)
+
+plt.xticks(())
+plt.yticks(())
+
+
+plt.figure()
+#3d图
+
+fig = plt.figure()
+ax = Axes3D(fig)
+# X, Y value
+X = np.arange(-4, 4, 0.25)
+Y = np.arange(-4, 4, 0.25)
+X, Y = np.meshgrid(X, Y)
+R = np.sqrt(X ** 2 + Y ** 2)
+# height value
+Z = np.sin(R)
+
+ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=plt.get_cmap('rainbow'))
+
+# I think this is different from plt12_contours
+ax.contourf(X, Y, Z, zdir='z', offset=-2, cmap=plt.get_cmap('rainbow'))
+
+ax.set_zlim(-2, 2)
 
 plt.show()
