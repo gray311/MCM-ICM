c20851f796ac13f075128332566685d0b72bb492
Yukinoshita Yukino <64787301+gray311@users.noreply.github.com>
2021-01-26 22:37:54 +0800

    Add files via upload

--- machine learning.py
+++ machine learning.py
@@ -10,6 +10,7 @@ from sklearn.datasets.samples_generator import make_classification
 from sklearn.svm import SVC  #处理分类问题的model
 from sklearn.model_selection import cross_val_score  #测试数据挑选
 from sklearn.model_selection import learning_curve
+from sklearn.model_selection import validation_curve
 from sklearn.datasets import load_digits
 
 
@@ -113,12 +114,27 @@ train_sizes, train_loss, test_loss = learning_curve(SVC(gamma=0.01), X, y, cv=10
 train_loss_mean = -np.mean(train_loss, axis=1)
 test_loss_mean = -np.mean(test_loss, axis=1)
 
-
-
 plt.plot(train_sizes, train_loss_mean, 'o-', color='r', label='Training')
 plt.plot(train_sizes, test_loss_mean, 'o-', color='g', label='Crossing-validation')
 
 plt.xlabel("Training examples")
 plt.ylabel("Loss")
 plt.legend(loc='best')
+plt.show()
+
+param_range = np.logspace(-6, -2.3, 5)
+train_loss, test_loss = validation_curve(
+        SVC(), X, y, param_name='gamma', param_range=param_range, cv=10,
+        scoring='neg_mean_squared_error')
+train_loss_mean = -np.mean(train_loss, axis=1)
+test_loss_mean = -np.mean(test_loss, axis=1)
+
+plt.plot(param_range, train_loss_mean, 'o-', color="r",
+             label="Training")
+plt.plot(param_range, test_loss_mean, 'o-', color="g",
+             label="Cross-validation")
+
+plt.xlabel("gamma")
+plt.ylabel("Loss")
+plt.legend(loc="best")
 plt.show()
